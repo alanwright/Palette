1b74ee3cf3d5141b775c58f145466c10ee846d5d
Alan Wright <alanwright47@gmail.com>
2014-06-02 16:54:43 -0400

    Basic Working Version

---
+++ js/index-main.js
@@ -0,0 +1,67 @@
+$(function(){
+	$('#input1').val('0E90D2');
+	$('#input2').val('8058A5');
+	$('#input3').val('5EB95E');
+	$('#input4').val('DD514C');
+	$('#input5').val('FAD232');
+
+	paletteListeners();
+});
+
+function blockEnter() {
+  return !(window.event && window.event.keyCode == 13); 
+}
+
+function paletteListeners() {
+	$("#input1").keyup(function() {
+		changeColor(1);
+	});
+	$("#input1").change(function() {
+		changeColor(1);
+	});
+	$("#input2").keyup(function() {
+		changeColor(2);
+	});
+	$("#input2").change(function() {
+		changeColor(2);
+	});
+	$("#input3").keyup(function() {
+		changeColor(3);
+	});
+	$("#input3").change(function() {
+		changeColor(3);
+	});
+	$("#input4").keyup(function() {
+		changeColor(4);
+	});
+	$("#input4").change(function() {
+		changeColor(4);
+	});
+	$("#input5").keyup(function() {
+		changeColor(5);
+	});
+	$("#input5").change(function() {
+		changeColor(5);
+	});
+	$("#input6").keyup(function() {
+		changeColor(6);
+	});
+	$("#input6").change(function() {
+		changeColor(6);
+	});
+
+	$('#create').click(function() {
+		var url = createURL();
+		window.open(url, '_blank');
+	});
+}
+
+function changeColor(num){
+	var color = $('#input' + num).val();
+	if( color.length == 3 || color.length == 6)
+		$('.color' + num).css('background-color', '#' + color);
+}
+
+function createURL() {
+	return 'palette.html?c1=' + $('#input1').val() + '&c2=' + $('#input2').val() + '&c3=' + $('#input3').val() + '&c4=' + $('#input4').val() + '&c5=' + $('#input5').val() + '&';
+}
--- js/palette-main.js
+++ js/palette-main.js
@@ -3,18 +3,23 @@ $(function(){
 
 	if (qs.c1 !== 'undefined' && qs.c1) {
 		$('.color1').css('background-color', '#' + qs.c1);
+		$('#hex1').html('#' + qs.c1.toUpperCase());
 	}
 	if (qs.c2 !== 'undefined' && qs.c2) {
 		$('.color2').css('background-color', '#' + qs.c2);
+		$('#hex2').html('#' + qs.c2.toUpperCase());
 	}
 	if (qs.c3 !== 'undefined' && qs.c3) {
 		$('.color3').css('background-color', '#' + qs.c3);
+		$('#hex3').html('#' + qs.c3.toUpperCase());
 	}
 	if (qs.c4 !== 'undefined' && qs.c4) {
 		$('.color4').css('background-color', '#' + qs.c4);
+		$('#hex4').html('#' + qs.c4.toUpperCase());
 	}
 	if (qs.c5 !== 'undefined' && qs.c5) {
 		$('.color5').css('background-color', '#' + qs.c5);
+		$('#hex5').html('#' + qs.c5.toUpperCase());
 	}
 });
 
